1d23e3f4edb537db1b94b152be77f1a673b98725
jack96olsz <jack96olsz@gmail.com>
2018-09-21 00:30:04 -0400

    installed react-native-web and apollo provider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ApolloProviderApp = () => {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+}
+
+const client = new ApolloClient({
+  // uri: "/graphql"
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+ReactDOM.render(ApolloProviderApp(), document.getElementById('root'));
 registerServiceWorker();
